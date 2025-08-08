d1074d6ee000c6f52002c5fc64cd15be05050554
[dyad] <m.hamid.hashemi@gmail.com>
2025-08-08 11:01:13 +0200

    Updated src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyB6VqRRzszy72LY3fbJywmaZIItfVMCjsw",
   authDomain: "sharesoocial.firebaseapp.com",
   projectId: "sharesoocial",
-  appId: "YOUR_APP_ID",
+  storageBucket: "sharesoocial.firebasestorage.app",
+  messagingSenderId: "206574095054",
+  appId: "1:206574095054:web:bbe951e376753d4ca85e9f",
+  measurementId: "G-995MPQSXW7"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
